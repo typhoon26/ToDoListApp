650889be699ae0beec1513f3916270369c819ec0
Ashwin Natarajan <ashwin0200@gmail.com>
2021-02-28 12:17:34 +0530

    Add .gitignore,Procfiles and update parts

--- app.js
+++ app.js
@@ -179,7 +179,10 @@ app.get("/:listcategory", function(req, res) {
 app.get("/about", function(req, res) {
   res.render("about");
 });
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port= process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
+app.listen(port, function() {
+  console.log("Server started");
 });
